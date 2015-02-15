35137852e3744050124b1a2318fec411ddb467b4
Julian DeFronzo <julian.defronzo@gmail.com>
2015-02-15 09:35:25 -0500

    Issue #4: Error handling if cannot get feed

--- lamp_post.py
+++ lamp_post.py
@@ -21,25 +21,29 @@ def get_times(api, stop):
     url = "http://realtime.mbta.com/developer/api/v2/predictionsbystop"
     opener = urllib2.build_opener()
     urllib2.install_opener(opener)
-
     params = {'api_key': api, 'stop': stop, 'format': "json"}
-
     data = urllib.urlencode(params)
-    response = urllib2.urlopen("%s?%s" %(url, data)).read()
-    parsed = json.loads(response)
+    try:
+        response = urllib2.urlopen("%s?%s" %(url, data)).read()
+        parsed = json.loads(response)
 
-    predictions = dict()
-    for i in parsed['mode']:
-        for r in i['route']:
-            for d in r['direction']:
-                if d['direction_id'] == "0":
-                    for p in d['trip']:
-                        heading = p['trip_headsign']
-                        mins, secs = str(datetime.timedelta(seconds=int(p['pre_away']))).split(":")[1:]
-                        pre_time = "%s:%s min" %(mins, secs)
-                        times = predictions.setdefault(heading, list())
-                        times.append(pre_time)
-    return predictions
+        predictions = dict()
+        for i in parsed['mode']:
+            for r in i['route']:
+                for d in r['direction']:
+                    if d['direction_id'] == "0":
+                        for p in d['trip']:
+                            heading = p['trip_headsign']
+                            mins, secs = str(datetime.timedelta(seconds=int(p['pre_away']))).split(":")[1:]
+                            pre_time = "%s:%s min" %(mins, secs)
+                            times = predictions.setdefault(heading, list())
+                            times.append(pre_time)
+        for heading, times in predictions.iteritems():
+            mins = [str(x) for x in times][0:2]
+            h_times.append("%s: %s" %(heading, " & ".join(mins)))
+        return h_times
+    except urllib2.HTTPError:
+        return ["No Predictions"]
 
 def get_alerts(api, stop):
     alerts = {}
@@ -49,12 +53,15 @@ def get_alerts(api, stop):
     urllib2.install_opener(opener)
     params = {'api_key': api, 'stop': stop, 'format': "json", 'include_access_alerts': "false"}
     data = urllib.urlencode(params)
-    response = urllib2.urlopen("%s?%s" %(url, data)).read()
-    parsed = json.loads(response)
-    alerts = parsed
-    if alerts.has_key('alert_headers'):
-        alert_headers = [i['header_text'] for i in alerts['alert_headers'] if i.has_key('header_text')]
-    return alert_headers
+    try:
+        response = urllib2.urlopen("%s?%s" %(url, data)).read()
+        parsed = json.loads(response)
+        alerts = parsed
+        if alerts.has_key('alert_headers'):
+            alert_headers = [i['header_text'] for i in alerts['alert_headers'] if i.has_key('header_text')]
+        return alert_headers
+    except urllib2.HTTPError:
+        return [""]
 
 def get_weather(api_key, lat, lng):
     w = forecastio.load_forecast(api_key, lat, lng)
@@ -114,9 +121,6 @@ def display():
         cache.set('weather', weather, timeout=60*60)
     ct, hs, wd = weather
 
-    for heading, times in predictions.iteritems():
-        mins = [str(x) for x in times][0:2]
-        h_times.append("%s: %s" %(heading, " & ".join(mins)))
     
     news = cache.get('news')
     if news is None:
@@ -128,7 +132,7 @@ def display():
         front_page = get_reddit_fp()
         cache.set('reddit_front_page', front_page, timeout=60*60)
 
-    return render_template('index.html', h_times=h_times, ct=ct, hs=hs, wd=wd, news=news, alerts=alerts, front_page=front_page)
+    return render_template('index.html', predictions=predictions, ct=ct, hs=hs, wd=wd, news=news, alerts=alerts, front_page=front_page)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
